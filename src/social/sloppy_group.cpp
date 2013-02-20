176aea0035688a9b31ce55c926d2e55e96f64aec
Jernej Kos <k@jst.sm>
2013-02-20 15:01:03 +0100

    Started work on the sloppy group manager.

---
+++ src/social/sloppy_group.cpp
@@ -0,0 +1,89 @@
+/*
+ * This file is part of UNISPHERE.
+ *
+ * Copyright (C) 2013 Jernej Kos <k@jst.sm>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "social/sloppy_group.h"
+#include "social/compact_router.h"
+
+namespace UniSphere {
+
+SloppyPeer::SloppyPeer(NameRecordPtr record)
+  : nodeId(record->nodeId),
+    addresses(record->addresses)
+{
+}
+
+LandmarkAddress SloppyPeer::landmarkAddress() const
+{
+  if (addresses.empty())
+    return LandmarkAddress();
+
+  return addresses.front();
+}
+
+bool SloppyPeer::operator<(const SloppyPeer &other) const
+{
+  return nodeId < other.nodeId;
+}
+
+SloppyGroupManager::SloppyGroupManager(CompactRouter &router, NetworkSizeEstimator &sizeEstimator)
+  : m_router(router),
+    m_sizeEstimator(sizeEstimator),
+    m_neighborRefreshTimer(router.context().service())
+{
+}
+
+void SloppyGroupManager::initialize()
+{
+
+}
+
+void SloppyGroupManager::shutdown()
+{
+
+}
+
+void SloppyGroupManager::refreshNeighborSet(const boost::system::error_code &error)
+{
+  if (error)
+    return;
+
+  RecursiveUniqueLock lock(m_mutex);
+  NameDatabase &ndb = m_router.nameDb();
+  m_oldNeighbors = m_neighbors;
+
+  // Lookup successor and predecessor
+  ndb.remoteLookupClosest(m_router.identity().localId(), true, boost::bind(&SloppyGroupManager::ndbHandleResponse, this, _1));
+
+  for (int i = 0; i < SloppyGroupManager::finger_count; i++) {
+    NodeIdentifier fingerId;
+    // TODO: Generate random finger identifier in our sloppy group
+    ndb.remoteLookupClosest(fingerId, false, boost::bind(&SloppyGroupManager::ndbHandleResponse, this, _1));
+  }
+}
+
+void SloppyGroupManager::ndbHandleResponse(const std::list<NameRecordPtr> &records)
+{
+  RecursiveUniqueLock lock(m_mutex);
+
+  // TODO: LRU set
+  for (NameRecordPtr record : records) {
+    m_neighbors.insert(SloppyPeer(record));
+  }
+}
+
+}
---
+++ src/social/sloppy_group.h
@@ -0,0 +1,87 @@
+/*
+ * This file is part of UNISPHERE.
+ *
+ * Copyright (C) 2013 Jernej Kos <k@jst.sm>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef UNISPHERE_SOCIAL_SLOPPYGROUP_H
+#define UNISPHERE_SOCIAL_SLOPPYGROUP_H
+
+#include <set>
+#include <boost/asio.hpp>
+
+#include "identity/node_identifier.h"
+#include "social/address.h"
+#include "social/name_database.h"
+
+namespace UniSphere {
+
+class CompactRouter;
+class NetworkSizeEstimator;
+
+class UNISPHERE_EXPORT SloppyPeer {
+public:
+  /**
+   * Constructs a sloppy peer from a name record.
+   *
+   * @param record Name record pointer
+   */
+  explicit SloppyPeer(NameRecordPtr record);
+
+  LandmarkAddress landmarkAddress() const;
+
+  bool operator<(const SloppyPeer &other) const;
+public:
+  /// Sloppy peer node identifier
+  NodeIdentifier nodeId;
+  /// A list of L-R addresses for this sloppy peer
+  std::list<LandmarkAddress> addresses;
+};
+
+/**
+ * Represents the sloppy group overlay manager.
+ */
+class UNISPHERE_EXPORT SloppyGroupManager {
+public:
+  /// Number of long-distance fingers to establish
+  static const int finger_count = 1;
+
+  SloppyGroupManager(CompactRouter &router, NetworkSizeEstimator &sizeEstimator);
+
+  void initialize();
+
+  void shutdown();
+protected:
+  void refreshNeighborSet(const boost::system::error_code &error);
+
+  void ndbHandleResponse(const std::list<NameRecordPtr> &records);
+private:
+  /// Router instance
+  CompactRouter &m_router;
+  /// Network size estimator
+  NetworkSizeEstimator &m_sizeEstimator;
+  /// Mutex protecting the sloppy group manager
+  mutable std::recursive_mutex m_mutex;
+  /// Neighbors in the overlay
+  std::set<SloppyPeer> m_neighbors;
+  /// Neighbors in the overlay before last refresh
+  std::set<SloppyPeer> m_oldNeighbors;
+  /// Timer for periodic neighbor set refresh
+  boost::asio::deadline_timer m_neighborRefreshTimer;
+};
+
+}
+
+#endif
