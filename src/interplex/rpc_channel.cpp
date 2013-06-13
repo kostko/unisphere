67e6de396c175ba2bc875dbc044d766cda1d4620
Jernej Kos <k@jst.sm>
2013-06-13 12:13:49 +0200

    Added Interplex direct RPC channel.

---
+++ src/interplex/rpc_channel.cpp
@@ -0,0 +1,91 @@
+/*
+ * This file is part of UNISPHERE.
+ *
+ * Copyright (C) 2013 Jernej Kos <k@jst.sm>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "interplex/rpc_channel.h"
+#include "interplex/link_manager.h"
+#include "core/context.h"
+
+namespace UniSphere {
+
+class InterplexRpcChannelPrivate {
+public:
+  /**
+   * Class constructor.
+   */
+  InterplexRpcChannelPrivate(InterplexRpcChannel &channel,
+                             LinkManager &manager);
+
+  /**
+   * Called by the router when a message is to be delivered to the local
+   * node.
+   */
+  void messageDelivery(const Message &msg);
+public:
+  UNISPHERE_DECLARE_PUBLIC(InterplexRpcChannel)
+
+  /// Link manager instance
+  LinkManager &m_manager;
+};
+
+InterplexRpcChannelPrivate::InterplexRpcChannelPrivate(InterplexRpcChannel &channel,
+                                                       LinkManager &manager)
+  : q(channel),
+    m_manager(manager)
+{
+  manager.signalMessageReceived.connect(boost::bind(&InterplexRpcChannelPrivate::messageDelivery, this, _1));
+}
+
+void InterplexRpcChannelPrivate::messageDelivery(const Message &msg)
+{
+  switch (msg.type()) {
+    case Message::Type::Interplex_RPC_Request: {
+      Protocol::RpcRequest request = message_cast<Protocol::RpcRequest>(msg);
+      q.signalDeliverRequest(request, msg);
+      break;
+    }
+    case Message::Type::Interplex_RPC_Response: {
+      Protocol::RpcResponse response = message_cast<Protocol::RpcResponse>(msg);
+      q.signalDeliverResponse(response, msg);
+      break;
+    }
+  }
+}
+
+InterplexRpcChannel::InterplexRpcChannel(LinkManager &manager)
+  : d(new InterplexRpcChannelPrivate(*this, manager)),
+    RpcChannel(manager.context())
+{
+}
+
+void InterplexRpcChannel::respond(const Message &msg,
+                                  const Protocol::RpcResponse &response,
+                                  const InterplexOptions &opts)
+{
+  // Send the RPC message back to the source node
+  d->m_manager.send(msg.originator(), Message(Message::Type::Interplex_RPC_Response, response));
+}
+
+void InterplexRpcChannel::request(const NodeIdentifier &destination,
+                                  const Protocol::RpcRequest &request,
+                                  const InterplexOptions &opts)
+{
+  // Send the RPC message
+  d->m_manager.send(destination, Message(Message::Type::Interplex_RPC_Request, request));
+}
+
+}
---
+++ src/interplex/rpc_channel.h
@@ -0,0 +1,70 @@
+/*
+ * This file is part of UNISPHERE.
+ *
+ * Copyright (C) 2013 Jernej Kos <k@jst.sm>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef UNISPHERE_INTERPLEX_RPCCHANNEL_H
+#define UNISPHERE_INTERPLEX_RPCCHANNEL_H
+
+#include "rpc/channel.hpp"
+#include "interplex/message.h"
+
+namespace UniSphere {
+
+class LinkManager;
+class InterplexOptions {};
+
+/**
+ * The interplex RPC channel can be used to perform RPC requests over
+ * the direct link between two peers.
+ */
+class UNISPHERE_EXPORT InterplexRpcChannel : public RpcChannel<Message, InterplexOptions> {
+public:
+  /**
+   * Class constructor.
+   *
+   * @param manager Link manager instance
+   */
+  InterplexRpcChannel(LinkManager &manager);
+
+  /**
+   * Sends a response back to request originator.
+   *
+   * @param msg Source message
+   * @param response RPC response to send back
+   * @param opts Channel-specific options
+   */
+  void respond(const Message &msg,
+               const Protocol::RpcResponse &response,
+               const InterplexOptions &opts = InterplexOptions());
+
+  /**
+   * Sends a request to a remote node.
+   *
+   * @param destination Destination node identifier
+   * @param request RPC request to send
+   * @param opts Channel-specific options
+   */
+  void request(const NodeIdentifier &destination,
+               const Protocol::RpcRequest &request,
+               const InterplexOptions &opts = InterplexOptions());
+private:
+  UNISPHERE_DECLARE_PRIVATE(InterplexRpcChannel)
+};
+
+}
+
+#endif
