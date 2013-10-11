ce3e29b844c726867ba08b647e150cbf313a6b3b
Jernej Kos <k@jst.sm>
2013-10-11 23:50:13 +0200

    Updated e-mail address.

--- src/social/name_database.h
+++ src/social/name_database.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of UNISPHERE.
  *
- * Copyright (C) 2013 Jernej Kos <k@jst.sm>
+ * Copyright (C) 2013 Jernej Kos <jernej@kos.mx>
  *
  * This library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
